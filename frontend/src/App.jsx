68bc10bf4976d67caca4b0301768e3f670cfe749
julesnuggy <julesnuggy@gmail.com>
2020-11-08 16:39:36 +0000

    Add react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import Home from './screens/Home'
 import styles from './App.module.scss'
 
 const App = () => (
-  <div className={styles.App}>
-    <Home />
-  </div>
+  <Router>
+    <Switch exact path='/'>
+      <Home />
+    </Switch>
+  </Router>
 )
 
 export default App
